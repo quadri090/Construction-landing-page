eeef087b794d06e130f0264995b630786fc8fd86
quadri090 <quadri090@gmail.com>
2023-08-15 21:21:00 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+         <h1 className="text-3xl font-bold text-blue-800 underline">Hello world!</h1>
+    </>
+  )
+}
+
+export default App
