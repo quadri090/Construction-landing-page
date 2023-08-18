fe8d4c16c79f78d7867cd44057cb797feb573c2c
quadri090 <quadri090@gmail.com>
2023-08-18 11:34:09 +0100

    refactored

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function MisVis(props) {
         <>
             <div className='w-[320px] md:w-[380px] h-[126px] md:-[166px] flex flex-col justify-between border-[2px] border-my-outline-green rounded-[4px] p-[12px] mx-auto'>
                 <div className='flex items-center w-[152px] md:w-[162px] h-[30px] gap-[10px]'>
-                    <img src={`${props.src}`} alt="icon" />
+                    <img src={`/src/img/${props.src}.png`} alt="icon" />
                     <p className='text-[18px] md:text-[20px] font-medium'>{props.misvis}</p>
                 </div>
                 <div className='h-[48px] md:text-[20px] font-medium leading-[24px] mt-[8px]'>
@@ -21,7 +21,7 @@ function Leaders(props) {
         <>
             <div className='w-[180px] h-[273px] text-center'>
                 <div className='flex justify-center items-center'>
-                    <img src={`${props.src}`} className='w-[169px] h-[179px]' alt="" />
+                    <img src={`/src/img/${props.src}.png`} className='w-[169px] h-[179px]' alt="An image" />
                 </div>
                 <p className='text-[18px] font-medium mt-[8px]'>{props.name}</p>
                 <p className='mt-[] leading-[24px] font-light'>{props.position}</p>
@@ -43,11 +43,11 @@ export default function About() {
                 </div>
                 <div className='w-[359px] md:w-[1140px] h-[286px] md:h-[126px] flex flex-col md:flex-row justify-between items-center mx-auto mt-[25px]'>
                     <MisVis
-                    src='/src/img/goal.png'
+                    src='goal'
                     misvis='Our Mission:'
                     desc='Build durable and eco-friendly buildings that will serve generations to come.' />
                     <MisVis
-                    src='/src/img/telescope.png'
+                    src='telescope'
                     misvis='Our Vision:'
                     desc='Being a world class organization that transforms lifes one building at a time.' />
                 </div>
@@ -56,15 +56,15 @@ export default function About() {
                 </div>
                 <div className='flex flex-col md:flex-row items-center justify-between w-[359px] md:w-[1128px] h-[869px] md:h-[353px] m-auto pb-[20px]'>
                     <Leaders
-                    src='/src/img/polygon 1.png'
+                    src='polygon 1'
                     name='Janet Juma'
                     position='CEO and Co-founder' />
                     <Leaders
-                    src='/src/img/polygon 2.png'
+                    src='polygon 2'
                     name='Hilary Doe'
                     position='Chief Architect' />
                     <Leaders
-                    src='/src/img/polygon 3.png'
+                    src='polygon 3'
                     name='John James'
                     position='Head of Marketing' />
                 </div>
